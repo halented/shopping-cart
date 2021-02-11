8260f5b362de12e18e0544c98448c9f2cdc730b7
Hal Dunn <heyhal19@gmail.com>
2021-02-11 14:36:19 -0800

    first

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import { useQuery } from 'react-query'
+// components
+import Drawer from '@material-ui/core/Drawer'
+import LinearProgress from '@material-ui/core/LinearProgress'
+import Grid from '@material-ui/core/Grid'
+import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart'
+import Badge from '@material-ui/core/Badge'
+// styles
+import { Wrapper } from './App.styles'
+// types
+export type CartItemType = {
+  id: number;
+  category: string;
+  description: string;
+  image: string;
+  price: number;
+  title: string;
+  amount: number;
+}
 
-function App() {
+
+const getProduces = async (): Promise<CartItemType> => {
+  await (await fetch('http:s//fakestoreapi.com/products')).json()
+}
+
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      heybag
     </div>
   );
 }
